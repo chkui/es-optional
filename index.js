056330ba13471b2e30ee180942c7d890ac5be0fa
chenkui <chenkui@chkui.com>
2018-05-22 14:02:42 +0800

    add new code

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fluent = undefined;
+
+var _fluent = require('./lib/fluent');
+
+exports.fluent = _fluent.fluent;
